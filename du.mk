f8dcb9ca7a9f5f7dee6b9e631cca4efbee772991
Vincenzo <vinz.mart@gmail.com>
2015-06-23 21:28:03 +0200

    Adapt for DirtyUnicorns

--- du.mk
+++ du.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,5 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
-add_lunch_combo aosp_maguro-userdebug
+$(call inherit-product, vendor/du/config/common.mk)
+
+PRODUCT_NAME := du_maguro
+
+PRODUCT_PACKAGES += \
+    Launcher3
